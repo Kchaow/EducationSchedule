c01716469657f4da8b5e7110d47153fe30bf404b
Alexey Letunov <xnihonsamuraix@gmail.com>
2024-03-18 12:13:17 +0300

    Add week number select

--- src/main/webapp/static/schedule.js
+++ src/main/webapp/static/schedule.js
@@ -1,7 +1,35 @@
+let weekNumber = 1;
+
 window.onload = function ()
 {
     let select = document.querySelector('[name="group-select"]');
+    let prevButton = document.querySelector('.prev-button');
+    let nextButton = document.querySelector('.next-button');
+    nextButton.addEventListener("click", incrementWeekNumber);
+    prevButton.addEventListener("click", decrementWeekNumber);
     select.addEventListener("change", getClasses);
+}
+
+function incrementWeekNumber()
+{
+    if (weekNumber < 16)
+    {
+        weekNumber++;
+    }
+    document.querySelector('.current-week-number').textContent = weekNumber;
+    if (document.querySelector('[name="group-select"]').value !== "default")
+        getClasses();
+}
+
+function decrementWeekNumber()
+{
+    if (weekNumber > 1)
+    {
+        weekNumber--;
+    }
+    document.querySelector('.current-week-number').textContent = weekNumber;
+    if (document.querySelector('[name="group-select"]').value !== "default")
+        getClasses();
 }
 
 async function getClasses()
@@ -11,16 +39,16 @@ async function getClasses()
     let group = groupInput.value;
     if (group === "default")
         return;
-    console.log(group);
-    let url = `http://localhost:8888/EducationSchedule/schedule/${group}/1`;
+    let url = `http://localhost:8888/EducationSchedule/schedule/${group}/${weekNumber}`;
     let response = await fetch(url);
-
     if (response.ok) 
     { 
         let json = await response.json();
         let days = document.querySelectorAll('.schedule-day');
         json.classes.forEach(el => {
-            let day = new Date(el.date.replace('/', '-')).getDay() - 1;
+            let date = new Date(el.date.replace('/', '-'));
+            let day = date.getDay() - 1;
+            days[day].querySelector('.class-date').textContent = `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
             if (day >= 0 && day <= 6)
             {
                 let clazz = days[day].querySelector(`[data-stroke-number="${el.classNumber}"]`);
@@ -37,12 +65,14 @@ async function getClasses()
 
     function clearStrokes()
     {
-        teachers = document.querySelectorAll('[data-teacher]');
-        subjects = document.querySelectorAll('[data-subject-name]');
-        strokes = document.querySelectorAll('[data-stroke-audience]');
+        let dates = document.querySelectorAll('.class-date');
+        let teachers = document.querySelectorAll('[data-teacher]');
+        let subjects = document.querySelectorAll('[data-subject-name]');
+        let strokes = document.querySelectorAll('[data-stroke-audience]');
 
         teachers.forEach(el => el.textContent = "");
         subjects.forEach(el => el.textContent = "");
         strokes.forEach(el => el.textContent = "");
+        dates.forEach(el => el.textContent = "");
     }
 }
